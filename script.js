8c6a06a003cd7c7809117ed99b9096699b32fb60
it63070160 <71114745+it63070160@users.noreply.github.com>
2021-05-24 00:46:54 +0700

    Update script.js
    fix image slideshow

--- script.js
+++ script.js
@@ -397,8 +397,6 @@ function calculate(){
     document.getElementById("calculated-save").innerHTML = 'ประหยัด: ' + save + unit_save;
 }
 // -- //
-
-// img slideshow
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -407,23 +405,13 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("imghere");
-  var dots = document.getElementsByClassName("selectimg");
+  var slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
